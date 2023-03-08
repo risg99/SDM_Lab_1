e4be8600b759fed4ed7e749f97bb501d20ea4d60
risg99 <rishikagupta1999@gmail.com>
2023-03-08 19:21:45 +0100

    Part B and C

---
+++ programs/queries_neo4j.py
@@ -0,0 +1,117 @@
+import pprint
+from session_helper_neo4j import create_session
+
+session = create_session()
+
+# Printing query results and summary 
+def print_query_results(records, summary):
+    pp = pprint.PrettyPrinter(indent = 4)
+    
+    print("The query `{query}` returned {records_count} records in {time} ms.".format(
+        query = summary.query, records_count = len(records),
+        time = summary.result_available_after,
+    ))
+
+    for record in records:
+        pp.pprint(record.data())
+        print()
+
+
+# Query 1: Top 3 most cited papers per conference
+def query_top3_cited_papers_conference(session):
+    result = session.run(
+        """MATCH (p:Paper) - [r1:cites] -> (citedPaper:Paper) - [r2:presented_in] -> (c:Conference) 
+            WITH c, citedPaper, count(r1) AS numberOfCitations
+            ORDER BY c, numberOfCitations DESC
+            WITH c, COLLECT(citedPaper)[0..3] AS top3
+            WHERE top3[1].title <> 'None' AND top3[2].title <> 'None'
+            RETURN c.name as conference, 
+                   top3[0].title AS topCitedPaper1,
+                   top3[1].title AS topCitedPaper2,
+                   top3[2].title AS topCitedPaper3
+            ;"""
+        )
+    records = list(result)
+    summary = result.consume()
+    return records, summary
+
+# Query 2: Authors that have published in the same conference in at least 4 editions ------ returns no records as no editions data
+def query_authors_published_same_conference_4editions(session):
+    result = session.run(
+        """MATCH (a:Author) - [:writes] -> (p:Paper) - [:presented_in] -> (c:Conference)
+            WITH c.name as conferenceName, a, COUNT(DISTINCT c.edition) AS distinctEditions
+            WHERE distinctEditions >= 4
+            WITH conferenceName, a
+            MATCH (a2:Author) - [:writes] -> (p2:Paper) - [:presented_in] -> (c)
+            WHERE a2.ID <> a.ID
+            WITH conferenceName, a, a2, COUNT(DISTINCT c.edition) AS distinctEditions1
+            WHERE distinctEditions1 >= 4
+            RETURN conferenceName,
+                   a.name as author1_name,
+                   a.email as author1_email,
+                   a2.name as author2_name,
+                   a2.email as author2_email
+            LIMIT 5;"""
+    )
+    records = list(result)
+    summary = result.consume()
+    return records, summary
+
+# Query 3 Impact factor ----------- returns no records
+# Impact factor = Citations(year1) / Publications(year1) + Publications(year2)
+def query_impact_factor(session):
+    result = session.run(
+        """MATCH(p:Paper) - [r1:cites] -> (citedP:Paper) - [r2:published_in] -> (j:Journal) 
+            WITH j, r2.year as currYear, COUNT(r1) AS totalCitations
+            MATCH (p2:Paper) - [r3:published_in] -> (j)
+            WHERE r3.year = currYear - 1 OR r3.year = currYear - 2
+            WITH j, currYear, totalCitations, COUNT(r3) AS totalPublications
+            WHERE totalPublications > 0
+            RETURN j.name AS journalName,
+                    currYear AS yearOfPublication,
+                    toFloat(totalCitations)/totalPublications AS impactFactor
+            ORDER BY impactFactor DESC
+            LIMIT 5;"""
+    )
+    records = list(result)
+    summary = result.consume()
+    return records, summary
+
+# Query 4 H-Index ----- getting op of 3k+ rows
+# H-Index = atleast h publications have h citations
+
+def query_h_index(session):
+    result = session.run(
+        """MATCH(a:Author) - [r1:writes] -> (p1:Paper) - [r2:cites] -> (p2:Paper)
+            WITH a, p2, COLLECT(p1) as papers
+            WITH a, p2, RANGE(1, SIZE(papers)) AS listOfPapers
+            UNWIND listOfPapers AS lp
+            WITH a, lp AS currHIndex, count(p2) AS citedPapers
+            WHERE currHIndex <= citedPapers
+            RETURN a.name AS authorName,
+                   currHIndex as hIndex
+            ORDER BY currHIndex DESC
+            LIMIT 5;"""
+    )
+    records = list(result)
+    summary = result.consume()
+    return records, summary
+
+
+records, summary = session.execute_read(query_top3_cited_papers_conference)
+print('Query Result 1.........')
+print_query_results(records, summary)
+
+records, summary = session.execute_read(query_authors_published_same_conference_4editions)
+print('Query Result 2.........')
+print_query_results(records, summary)
+
+records, summary = session.execute_read(query_impact_factor)
+print('Query Result 3.........')
+print_query_results(records, summary)
+
+records, summary = session.execute_read(query_h_index)
+print('Query Result 4.........')
+print_query_results(records, summary)
+
+session.close()
---
+++ programs/recommender_neo4j.py
@@ -0,0 +1,166 @@
+import pprint
+from session_helper_neo4j import create_session
+
+session = create_session()
+
+# Printing query results and summary 
+def print_query_results(records, summary):
+    pp = pprint.PrettyPrinter(indent = 4)
+    
+    print("The query `{query}` returned {records_count} records in {time} ms.".format(
+        query = summary.query, records_count = len(records),
+        time = summary.result_available_after,
+    ))
+
+    for record in records:
+        pp.pprint(record.data())
+        print()
+
+# Query 1: Define research community
+def query_define_research_community(session):
+    session.run(
+        "MATCH (n: ResearchCommunity) DETACH DELETE n"
+    )
+
+    session.run(
+        """MERGE (rc:ResearchCommunity {name:"Databases"});"""
+    )
+
+def query_keywords_belongingTo_community(session):
+    result = session.run(
+        """MATCH(k:Keyword)
+            WHERE k.name IN ['Data Management', 'Indexing', 'Data Modeling', 'Big Data', 'Data Processing', 'Data Storage','Data Querying']
+            WITH k
+            MATCH(rc:ResearchCommunity {name:"Databases"})
+            MERGE(k) - [r1:belongs_to] -> (rc) 
+            RETURN k AS keyword, 
+                    r1 AS belongs_to, 
+                    rc AS researchCommunity
+            LIMIT 5;"""
+    )
+    records = list(result)
+    summary = result.consume()
+    return records, summary
+
+# Query 2: Identifying papers from conferences or journals belonging to the community
+def query_conference_journals_community(session):
+    result = session.run(
+        """MATCH (rc:ResearchCommunity {name:"Databases"}) <- [:belongs_to] - (k:Keyword) <- [:has*1..] - (cp:Paper) - [r1] -> (x)
+            WHERE x:Journal OR x:Conference AND x.title <> 'None' AND r1 IN ['published_in','presented_in']
+            WITH cp, COUNT(DISTINCT cp) AS numberOfCommunityPapers
+            MATCH (p:Paper) - [r2] -> (x)
+            WHERE x:Journal OR x:Conference AND r2 IN ['published_in','presented_in']
+            WITH p, cp, x, numberOfCommunityPapers, COUNT(DISTINCT p) AS numberOfPapers
+            WHERE (toFloat(numberOfCommunityPapers) / (numberOfPapers)) >= 0.9
+            RETURN p.title AS paperName, 
+                labels(x)[0] AS confJour, 
+                x.name AS nameOfConfJour
+            LIMIT 5;"""
+    )
+    records = list(result)
+    summary = result.consume()
+    return records, summary
+
+# Identifying the top 100 ranked papers wrt citations from the community
+def query_run_pageRank_algorithm(session):
+    print('Storing the graph from Part-2 into cypher catalog')
+    session.run("""CALL gds.graph.drop('graph1_papers_belongingTo_databases_community',false);""")
+
+    session.run(
+        """CALL gds.graph.project.cypher('graph1_papers_belongingTo_databases_community',    
+            'MATCH (rc:ResearchCommunity {name:"Databases"}) <- [:belongs_to] - (k:Keyword) <- [:has*1..] - (cp:Paper) - [r1:published_in|presented_in] -> (x)
+            WHERE x:Journal OR x:Conference
+            WITH cp, COUNT(DISTINCT cp) AS numberOfCommunityPapers
+            MATCH (p:Paper) - [r2:published_in|presented_in] -> (x)
+            WHERE x:Journal OR x:Conference
+            WITH p, cp, x, numberOfCommunityPapers, COUNT(DISTINCT p) AS numberOfPapers
+            WHERE (toFloat(numberOfCommunityPapers) / (numberOfPapers)) >= 0.9
+            RETURN id(p) AS id;',
+            'MATCH (p1:Paper) - [:cites] -> (p2:Paper)
+            RETURN id(p1) AS source,
+                   id(p2) AS target',           
+            {validateRelationships:FALSE});"""
+    )
+
+    print('Running the page-rank algorithm for the stored graph')
+    session.run(
+        """CALL gds.pageRank.write('graph1_papers_belongingTo_databases_community', {
+           maxIterations: 20,
+           dampingFactor: 0.85,
+           writeProperty: 'pagerank'
+           });"""
+    )
+
+def query_top100_papers_pageRank(session):
+    result = session.run(
+        """MATCH (rc:ResearchCommunity {name:"Databases"}) <- [:belongs_to] - (k:Keyword) <- [:has*1..] - (cp:Paper) - [r1:published_in|presented_in] -> (x)
+            WHERE x:Journal OR x:Conference
+            WITH cp, COUNT(DISTINCT cp) AS numberOfCommunityPapers
+            MATCH (p:Paper) - [r2:published_in|presented_in] -> (x)
+            WHERE x:Journal OR x:Conference
+            WITH p, cp, x, numberOfCommunityPapers, COUNT(DISTINCT p) AS numberOfPapers
+            WHERE (toFloat(numberOfCommunityPapers) / (numberOfPapers)) >= 0.9
+            WITH x, p
+            ORDER BY p.pagerank DESC
+            WITH x, COLLECT(p) AS p
+            RETURN labels(x)[0] AS confJour, x.name AS nameOfConfJour, p[0..100] AS top100Papers
+            ORDER BY x
+            LIMIT 5;"""
+    )
+    records = list(result)
+    summary = result.consume()
+    return records, summary
+
+# Identifying potential reviewers (gurus) who authored atleast 2 of top100 papers from Query 3
+def query_gurus_conferences(session):
+    result = session.run(
+        """MATCH (rc:ResearchCommunity {name:"Databases"}) <- [:belongs_to] - (k:Keyword) <- [:has*1..] - (cp:Paper) - [r1:published_in|presented_in] -> (x)
+            WHERE x:Journal OR x:Conference
+            WITH cp, COUNT(DISTINCT cp) AS numberOfCommunityPapers
+            MATCH (p:Paper) - [r2:published_in|presented_in] -> (x)
+            WHERE x:Journal OR x:Conference
+            WITH p, cp, x, numberOfCommunityPapers, COUNT(DISTINCT p) AS numberOfPapers
+            WHERE (toFloat(numberOfCommunityPapers) / (numberOfPapers)) >= 0.9
+            WITH x, p
+            ORDER BY p.pagerank DESC
+            WITH x, COLLECT(p) AS p
+            WITH labels(x)[0] AS confJour, x.name AS nameOfConfJour, p[0..100] AS top100Papers
+            UNWIND top100Papers AS topPapers
+            MATCH (a:Author) - [r3:writes] -> (p:topPapers)
+            WITH a, COUNT(r3) AS numberOfWrittenPapers
+            WHERE numberOfWrittenPapers >= 2
+            RETURN a.name AS potentialReviewerName,
+                a.email AS potentialReviewerEmail,
+                numberOfWrittenPapers AS guru
+            LIMIT 5;"""
+    )
+    records = list(result)
+    summary = result.consume()
+    return records, summary
+
+
+print('Part 1..........')    
+print('Creating Research Community node with name = Databases')
+session.execute_write(query_define_research_community)
+print('Creating relations from keywords to the Research Community')
+records, summary = session.execute_write(query_keywords_belongingTo_community)
+print_query_results(records, summary)
+
+
+print('Part 2..........')
+print('Finding papers from conferences or journals that belong to research community of databases')
+records, summary = session.execute_read(query_conference_journals_community)
+print_query_results(records, summary)
+
+print('Part 3..........')
+session.execute_write(query_run_pageRank_algorithm)
+print('Obtaining the results of page-rank')
+records, summary = session.execute_read(query_top100_papers_pageRank)
+print_query_results(records, summary)
+
+print('Part 4..........')
+print('Finding gurus of the top conferences and journals')
+records, summary = session.execute_read(query_gurus_conferences)
+print_query_results(records, summary)
+
+session.close()
